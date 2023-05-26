a725cc8c48ef1a0cecba2e53f282fbbea7e3b84d
Michael Salas <michaelsalasalcedo15@gmail.com>
2023-05-25 23:03:58 -0500

    config breadcrumbs and title

---
+++ src/app/pages/promesas/promesas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-promesas',
+  templateUrl: './promesas.component.html',
+  styleUrls: []
+})
+export class PromesasComponent {
+
+}
